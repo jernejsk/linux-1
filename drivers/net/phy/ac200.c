8668d244681a235f2d29503dc62029a94397fa2e
Jernej Skrabec <jernej.skrabec@siol.net>
2019-08-16 16:38:57 +0200

    net: phy: Add support for AC200 EPHY
    The X-Powers AC200 mixed signal chip contains a 100Mbit/s Ethernet PHY.
    While its sporting a usable default setup, and can be controlled by the
    generic IEEE802.3-C22 PHY driver, the BSP sets up some extra registers,
    which this driver here covers.
    
    Add a PHY driver matching the AC200 EPHY ID registers, and which
    programs some PHY registers according to the BSP code.
    
    Signed-off-by: Jernej Skrabec <jernej.skrabec@siol.net>
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

---
+++ drivers/net/phy/ac200.c
@@ -0,0 +1,193 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Driver for AC200 Ethernet PHY
+ *
+ * Copyright (c) 2019 Jernej Skrabec <jernej.skrabec@gmail.com>
+ */
+
+#include <linux/clk.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/phy.h>
+
+#define AC200_EPHY_ID			0x00441400
+#define AC200_EPHY_ID_MASK		0x0ffffff0
+
+/*
+ * The register names come from the vendor driver for the Rockchip RK630,
+ * which carries the same PHY, with the same ID and the same tuning values.
+ */
+
+/* page 0 registers */
+#define AC200_EPHY_GLOBAL_CTL		0x13
+#define AC200_EPHY_GLOBAL_CTL_UNKNOWN	BIT(12)
+#define AC200_EPHY_PAGE_SELECT		0x1f
+
+/* the page number lives in the upper byte of the page select register */
+#define AC200_EPHY_PAGE(x)		((x) << 8)
+
+/* page 1 registers */
+#define AC200_EPHY_P1_APS_CTL		0x12
+#define AC200_EPHY_P1_EEE_CTL		0x17
+#define AC200_EPHY_P1_EEE_INTELLIGENT	BIT(3)
+
+/* page 2 registers */
+#define AC200_EPHY_P2_AFE_CTL		0x18
+
+/* page 6 registers */
+#define AC200_EPHY_P6_AFE_RX_CTL	0x13
+#define AC200_EPHY_P6_AFE_TX_CTL	0x14
+#define AC200_EPHY_P6_AFE_DRIVER2	0x15
+
+/* page 8 registers */
+#define AC200_EPHY_P8_AFE_CTL		0x18
+
+struct ac200_ephy_priv {
+	struct clk *clk;
+};
+
+static int ac200_ephy_read_page(struct phy_device *phydev)
+{
+	return __phy_read(phydev, AC200_EPHY_PAGE_SELECT);
+}
+
+static int ac200_ephy_write_page(struct phy_device *phydev, int page)
+{
+	return __phy_write(phydev, AC200_EPHY_PAGE_SELECT, page);
+}
+
+static int ac200_ephy_config_init(struct phy_device *phydev)
+{
+	int ret;
+
+	/* Disable APS */
+	ret = phy_write_paged(phydev, AC200_EPHY_PAGE(1),
+			      AC200_EPHY_P1_APS_CTL, 0x4824);
+	if (ret)
+		return ret;
+
+	/* PHYAFE TRX optimization */
+	ret = phy_write_paged(phydev, AC200_EPHY_PAGE(2),
+			      AC200_EPHY_P2_AFE_CTL, 0x0000);
+	if (ret)
+		return ret;
+
+	/* PHYAFE TX optimization */
+	ret = phy_write_paged(phydev, AC200_EPHY_PAGE(6),
+			      AC200_EPHY_P6_AFE_TX_CTL, 0x708f);
+	if (ret)
+		return ret;
+
+	/* PHYAFE RX optimization */
+	ret = phy_write_paged(phydev, AC200_EPHY_PAGE(6),
+			      AC200_EPHY_P6_AFE_RX_CTL, 0xf000);
+	if (ret)
+		return ret;
+
+	ret = phy_write_paged(phydev, AC200_EPHY_PAGE(6),
+			      AC200_EPHY_P6_AFE_DRIVER2, 0x1530);
+	if (ret)
+		return ret;
+
+	/* PHYAFE TRX optimization */
+	ret = phy_write_paged(phydev, AC200_EPHY_PAGE(8),
+			      AC200_EPHY_P8_AFE_CTL, 0x00bc);
+	if (ret)
+		return ret;
+
+	/* Disable intelligent EEE. */
+	ret = phy_modify_paged(phydev, AC200_EPHY_PAGE(1),
+			       AC200_EPHY_P1_EEE_CTL,
+			       AC200_EPHY_P1_EEE_INTELLIGENT, 0);
+	if (ret)
+		return ret;
+
+	/* Disable 802.3az EEE. */
+	ret = phy_clear_bits_mmd(phydev, MDIO_MMD_AN, MDIO_AN_EEE_ADV,
+				 MDIO_EEE_100TX);
+	if (ret)
+		return ret;
+
+	/*
+	 * Undocumented, and only done by the vendor driver when the AC200 is
+	 * paired with an H6 (CONFIG_ARCH_SUN50IW6). That is the only SoC this
+	 * chip is used with, so apply it unconditionally.
+	 */
+	return phy_set_bits(phydev, AC200_EPHY_GLOBAL_CTL,
+			    AC200_EPHY_GLOBAL_CTL_UNKNOWN);
+}
+
+static int ac200_ephy_probe(struct phy_device *phydev)
+{
+	struct device *dev = &phydev->mdio.dev;
+	struct ac200_ephy_priv *priv;
+
+	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
+	if (!priv)
+		return -ENOMEM;
+
+	priv->clk = devm_clk_get_optional_enabled(dev, NULL);
+	if (IS_ERR(priv->clk))
+		return dev_err_probe(dev, PTR_ERR(priv->clk),
+				     "Failed to request clock\n");
+
+	phydev->priv = priv;
+
+	return 0;
+}
+
+static int ac200_ephy_suspend(struct phy_device *phydev)
+{
+	struct ac200_ephy_priv *priv = phydev->priv;
+	int ret;
+
+	ret = genphy_suspend(phydev);
+	if (ret)
+		return ret;
+
+	clk_disable_unprepare(priv->clk);
+
+	return 0;
+}
+
+static int ac200_ephy_resume(struct phy_device *phydev)
+{
+	struct ac200_ephy_priv *priv = phydev->priv;
+	int ret;
+
+	ret = clk_prepare_enable(priv->clk);
+	if (ret)
+		return ret;
+
+	ret = genphy_resume(phydev);
+	if (ret)
+		clk_disable_unprepare(priv->clk);
+
+	return ret;
+}
+
+static struct phy_driver ac200_ephy_driver[] = {
+	{
+		.phy_id		= AC200_EPHY_ID,
+		.phy_id_mask	= AC200_EPHY_ID_MASK,
+		.name		= "X-Powers AC200 EPHY",
+		.soft_reset	= genphy_soft_reset,
+		.config_init	= ac200_ephy_config_init,
+		.probe		= ac200_ephy_probe,
+		.read_page	= ac200_ephy_read_page,
+		.write_page	= ac200_ephy_write_page,
+		.suspend	= ac200_ephy_suspend,
+		.resume		= ac200_ephy_resume,
+	}
+};
+module_phy_driver(ac200_ephy_driver);
+
+MODULE_AUTHOR("Jernej Skrabec <jernej.skrabec@gmail.com>");
+MODULE_DESCRIPTION("AC200 Ethernet PHY driver");
+MODULE_LICENSE("GPL");
+
+static const struct mdio_device_id __maybe_unused ac200_ephy_phy_tbl[] = {
+	{ AC200_EPHY_ID, AC200_EPHY_ID_MASK },
+	{ }
+};
+MODULE_DEVICE_TABLE(mdio, ac200_ephy_phy_tbl);
