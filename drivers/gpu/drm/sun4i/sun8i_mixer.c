ba4319893ba4e08b88026fba590a9b8cc47f5c85
Jernej Skrabec <jernej.skrabec@gmail.com>
2026-03-15 11:15:14 +0100

    drm/sun4i: mixer: simplify init procedure
    There is no need to set all units enable bits to 0 as deasserting reset
    line does same thing automatically.
    
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>

--- drivers/gpu/drm/sun4i/sun8i_mixer.c
+++ drivers/gpu/drm/sun4i/sun8i_mixer.c
@@ -517,7 +517,7 @@ static int sun8i_mixer_bind(struct device *dev, struct device *master,
 	struct sun4i_drv *drv = drm->dev_private;
 	struct sun8i_mixer *mixer;
 	void __iomem *regs;
-	int i, ret;
+	int ret;
 
 	/*
 	 * The mixer uses single 32-bit register to store memory
@@ -660,34 +660,6 @@ static int sun8i_mixer_bind(struct device *dev, struct device *master,
 	}
 
 	list_add_tail(&mixer->engine.list, &drv->engine_list);
-
-	/* Reset registers and disable unused sub-engines */
-	if (mixer->cfg->de_type == SUN8I_MIXER_DE3) {
-		for (i = 0; i < DE3_MIXER_UNIT_SIZE; i += 4)
-			regmap_write(mixer->engine.regs, i, 0);
-
-		regmap_write(mixer->engine.regs, SUN50I_MIXER_FCE_EN, 0);
-		regmap_write(mixer->engine.regs, SUN50I_MIXER_PEAK_EN, 0);
-		regmap_write(mixer->engine.regs, SUN50I_MIXER_LCTI_EN, 0);
-		regmap_write(mixer->engine.regs, SUN50I_MIXER_BLS_EN, 0);
-		regmap_write(mixer->engine.regs, SUN50I_MIXER_FCC_EN, 0);
-		regmap_write(mixer->engine.regs, SUN50I_MIXER_DNS_EN, 0);
-		regmap_write(mixer->engine.regs, SUN50I_MIXER_DRC_EN, 0);
-		regmap_write(mixer->engine.regs, SUN50I_MIXER_FMT_EN, 0);
-		regmap_write(mixer->engine.regs, SUN50I_MIXER_CDC0_EN, 0);
-		regmap_write(mixer->engine.regs, SUN50I_MIXER_CDC1_EN, 0);
-	} else if (mixer->cfg->de_type == SUN8I_MIXER_DE2) {
-		for (i = 0; i < DE2_MIXER_UNIT_SIZE; i += 4)
-			regmap_write(mixer->engine.regs, i, 0);
-
-		regmap_write(mixer->engine.regs, SUN8I_MIXER_FCE_EN, 0);
-		regmap_write(mixer->engine.regs, SUN8I_MIXER_BWS_EN, 0);
-		regmap_write(mixer->engine.regs, SUN8I_MIXER_LTI_EN, 0);
-		regmap_write(mixer->engine.regs, SUN8I_MIXER_PEAK_EN, 0);
-		regmap_write(mixer->engine.regs, SUN8I_MIXER_ASE_EN, 0);
-		regmap_write(mixer->engine.regs, SUN8I_MIXER_FCC_EN, 0);
-		regmap_write(mixer->engine.regs, SUN8I_MIXER_DCSC_EN, 0);
-	}
 
 	sun8i_mixer_init(mixer);
 
