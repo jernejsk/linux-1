e93ad0d7cddc0a762e6cebfd669273d1ca5f70fd
Jernej Skrabec <jernej.skrabec@gmail.com>
2026-05-22 16:41:55 +0000

    drm/sun4i: tcon: Add A523 TCON LCD and TV compatibles
    A523 reuses the R40 TCON LCD register layout for TCON_LCD0/1/2 and
    the H616 TCON TV layout (pad_sel at BIT(1) of TCON_GCTL) for the
    HDMI / eDP TCONs. Bind both new compatibles to the respective quirks.
    
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>

--- drivers/gpu/drm/sun4i/sun4i_tcon.c
+++ drivers/gpu/drm/sun4i/sun4i_tcon.c
@@ -1583,6 +1583,8 @@ const struct of_device_id sun4i_tcon_of_table[] = {
 	{ .compatible = "allwinner,sun20i-d1-tcon-lcd", .data = &sun20i_d1_lcd_quirks },
 	{ .compatible = "allwinner,sun20i-d1-tcon-tv", .data = &sun8i_r40_tv_quirks },
 	{ .compatible = "allwinner,sun50i-h616-tcon-tv", .data = &sun50i_h616_tv_quirks },
+	{ .compatible = "allwinner,sun55i-a523-tcon-lcd", .data = &sun8i_r40_lcd_quirks },
+	{ .compatible = "allwinner,sun55i-a523-tcon-tv", .data = &sun50i_h616_tv_quirks },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, sun4i_tcon_of_table);
