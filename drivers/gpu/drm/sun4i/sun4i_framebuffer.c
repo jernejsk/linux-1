cc8e1885ec0d46b8b72558d000daa15abb7727df
Jernej Skrabec <jernej.skrabec@gmail.com>
2026-07-12 22:20:59 +0200

    drm/sun4i: Add DE3 writeback support
    Expose DE3 RT-WB as a DRM writeback connector.
    
    Support RGB and YUV420 output, scaling and interrupt completion.
    
    Use the input graph to identify the source mixer and CRTC.
    
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>

--- drivers/gpu/drm/sun4i/sun4i_framebuffer.c
+++ drivers/gpu/drm/sun4i/sun4i_framebuffer.c
@@ -36,8 +36,25 @@ static const struct drm_mode_config_funcs sun4i_de_mode_config_funcs = {
 	.fb_create		= drm_gem_fb_create,
 };
 
+static void sun4i_de_atomic_commit_tail(struct drm_atomic_commit *state)
+{
+	struct drm_device *dev = state->dev;
+
+	drm_atomic_helper_commit_modeset_disables(dev, state);
+	drm_atomic_helper_commit_crtc_enable(dev, state);
+	drm_atomic_helper_commit_encoder_bridge_pre_enable(dev, state);
+	drm_atomic_helper_commit_planes(dev, state,
+					DRM_PLANE_COMMIT_ACTIVE_ONLY);
+	drm_atomic_helper_commit_encoder_bridge_enable(dev, state);
+	drm_atomic_helper_commit_writebacks(dev, state);
+	drm_atomic_helper_fake_vblank(state);
+	drm_atomic_helper_commit_hw_done(state);
+	drm_atomic_helper_wait_for_flip_done(dev, state);
+	drm_atomic_helper_cleanup_planes(dev, state);
+}
+
 static const struct drm_mode_config_helper_funcs sun4i_de_mode_config_helpers = {
-	.atomic_commit_tail	= drm_atomic_helper_commit_tail_rpm,
+	.atomic_commit_tail	= sun4i_de_atomic_commit_tail,
 };
 
 void sun4i_framebuffer_init(struct drm_device *drm)
