62a2435e3f72729c4018ecb52880768c677dc60f
Jernej Skrabec <jernej.skrabec@gmail.com>
2026-07-19 15:12:04 +0200

    net: stmmac: dwmac-sun8i: Add support for H616 EMAC1
    The second H616 EMAC uses the clock configuration register at offset
    0x34 in the system control block instead of 0x30. Add a variant with
    the correct register field. Capabilities otherwise match the A64 EMAC.
    
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-sun8i.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-sun8i.c
@@ -81,6 +81,13 @@ static const struct reg_field sun8i_syscon_reg_field = {
 	.msb = 31,
 };
 
+/* EMAC1 clock register @ 0x34 in the "system control" address range */
+static const struct reg_field sun8i_syscon_emac1_reg_field = {
+	.reg = 0x34,
+	.lsb = 0,
+	.msb = 31,
+};
+
 /* EMAC clock register @ 0x164 in the CCU address range */
 static const struct reg_field sun8i_ccu_reg_field = {
 	.reg = 0x164,
@@ -135,6 +142,16 @@ static const struct emac_variant emac_variant_h6 = {
 	/* The "Internal PHY" of H6 is not on the die. It's on the
 	 * co-packaged AC200 chip instead.
 	 */
+	.soc_has_internal_phy = false,
+	.support_mii = true,
+	.support_rmii = true,
+	.support_rgmii = true,
+	.rx_delay_max = 31,
+	.tx_delay_max = 7,
+};
+
+static const struct emac_variant emac_variant_h616_emac1 = {
+	.syscon_field = &sun8i_syscon_emac1_reg_field,
 	.soc_has_internal_phy = false,
 	.support_mii = true,
 	.support_rmii = true,
@@ -1278,6 +1295,8 @@ static const struct of_device_id sun8i_dwmac_match[] = {
 		.data = &emac_variant_a64 },
 	{ .compatible = "allwinner,sun50i-h6-emac",
 		.data = &emac_variant_h6 },
+	{ .compatible = "allwinner,sun50i-h616-emac1",
+		.data = &emac_variant_h616_emac1 },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, sun8i_dwmac_match);
