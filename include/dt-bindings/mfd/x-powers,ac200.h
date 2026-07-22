20f08e6c3d031bea7faeaad1de13632e93e3181f
Jernej Skrabec <jernej.skrabec@gmail.com>
2026-07-22 07:08:02 +0200

    dt-bindings: mfd: Add X-Powers AC200
    Document the X-Powers AC200 mixed-signal companion IC and its supported
    Ethernet PHY control and audio codec children.
    
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>

---
+++ include/dt-bindings/mfd/x-powers,ac200.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Interrupt numbers of the X-Powers AC200 interrupt controller.
+ */
+
+#ifndef _DT_BINDINGS_MFD_X_POWERS_AC200_H
+#define _DT_BINDINGS_MFD_X_POWERS_AC200_H
+
+#define AC200_IRQ_TVE			0
+#define AC200_IRQ_EPHY			1
+#define AC200_IRQ_RTC			2
+
+#endif /* _DT_BINDINGS_MFD_X_POWERS_AC200_H */
