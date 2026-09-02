4b879609702a7f47ef3b94a142c1088094ba85d5
Jernej Skrabec <jernej.skrabec@gmail.com>
2026-09-02 22:12:08 +0200

    dt-bindings: clock: add the Allwinner A733 SerDes subsystem CCU
    The SerDes subsystem of the A733 hosts the USB3 and the PCIe controller.
    Its top level registers gate their bus clocks, select whether the USB3
    controller takes its PIPE clock from the combo PHY or from the USB2 side
    and hold the reset of the USB2 PHY that belongs to the USB3 controller.
    
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>

---
+++ include/dt-bindings/clock/sun60i-a733-serdes-ccu.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR MIT) */
+/*
+ * Copyright (C) 2026 Jernej Skrabec <jernej.skrabec@gmail.com>
+ */
+
+#ifndef _DT_BINDINGS_CLK_SUN60I_A733_SERDES_CCU_H_
+#define _DT_BINDINGS_CLK_SUN60I_A733_SERDES_CCU_H_
+
+#define CLK_SERDES_USB3_HCLK	0
+#define CLK_SERDES_USB3_ACLK	1
+#define CLK_SERDES_USB3_PIPE	2
+
+#endif /* _DT_BINDINGS_CLK_SUN60I_A733_SERDES_CCU_H_ */
---
+++ include/dt-bindings/reset/sun60i-a733-serdes-ccu.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR MIT) */
+/*
+ * Copyright (C) 2026 Jernej Skrabec <jernej.skrabec@gmail.com>
+ */
+
+#ifndef _DT_BINDINGS_RST_SUN60I_A733_SERDES_CCU_H_
+#define _DT_BINDINGS_RST_SUN60I_A733_SERDES_CCU_H_
+
+#define RST_SERDES_USB3_USB2_PHY	0
+
+#endif /* _DT_BINDINGS_RST_SUN60I_A733_SERDES_CCU_H_ */
