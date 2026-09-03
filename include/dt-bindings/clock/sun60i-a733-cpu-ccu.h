98aa36cc924de0c1da0d01152102da249ebd198a
Jernej Skrabec <jernej.skrabec@gmail.com>
2026-09-03 10:47:59 +0200

    dt-bindings: clock: add the Allwinner A733 CPU clock controller
    The A733 clocks its two CPU clusters and the DSU from a block separate
    from the CCU. It holds a linear PLL for each of the three, a generic
    backup PLL, and a clock selector per consumer that can also fall back to
    the oscillator or to a peripheral PLL output.
    
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>

---
+++ include/dt-bindings/clock/sun60i-a733-cpu-ccu.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR MIT) */
+/*
+ * Copyright (C) 2026 Jernej Skrabec <jernej.skrabec@gmail.com>
+ */
+
+#ifndef _DT_BINDINGS_CLK_SUN60I_A733_CPU_CCU_H_
+#define _DT_BINDINGS_CLK_SUN60I_A733_CPU_CCU_H_
+
+#define CLK_PLL_CPU_BACK	0
+#define CLK_PLL_CPU_L		1
+#define CLK_PLL_CPU_B		2
+#define CLK_PLL_CPU_DSU		3
+#define CLK_CPU_L		4
+#define CLK_CPU_B		5
+#define CLK_CPU_DSU		6
+
+#endif /* _DT_BINDINGS_CLK_SUN60I_A733_CPU_CCU_H_ */
